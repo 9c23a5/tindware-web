8f231bbd87d49b189142fe88a828b6e4a9fd9ae2
9c23a5 <40715125+9c23a5@users.noreply.github.com>
2021-11-25 09:58:25 +0100

    crear index.php y documentar header

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TindWare</title>
+</head>
+<body>
+    <header>
+        <div id="logo">
+            <!-- Un img con el logo de TindWare y un canvas con el nombre -->
+        </div>
+
+        <div id="menu">
+            <!-- Un menu con las diferentes opciones (mapa tecnicos, ultimas ofertas, faq, etc...) -->
+            <!-- Hay otros menus, por ejemplo 'Panel administracion', que dependen segun el valor de la cookie de inicio de sesion -->
+        </div>
+
+        <div id="login_registro">
+            <!-- Enlaces para Iniciar Sesión y Registro -->
+        </div>
+    </header>
+</body>
+</html>
