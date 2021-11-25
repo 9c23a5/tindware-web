052e6b4278e1c47ac16b5a7923f1fe9463395d9e
YesBoris <borjapachi@gmail.com>
2021-11-25 13:17:39 +0100

    Merge branch 'main' of https://github.com/9c23a5/tindware-web

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="misc.css" type="text/css"/>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,8 @@
 </head>
 <body>
     <?php
+    # Cargar constantes
+    require 'constantes.php';
     # Cargamos cookies
     require 'obtenerCookies.php';
     echo "MIS COOKIES SON $id_user Y $type_user"; # debug
@@ -96,12 +99,39 @@
             <!-- Hay otros menus, por ejemplo 'Panel administracion', que dependen segun el valor de la cookie de inicio de sesion -->
         </div>
 
-
+        <?php
+        # Si el usuario ha iniciado sesión escondemos este menu
+        if ($id_user != 0) {
+            echo HIDE;
+        }
+        ?>
         <div id="login_registro">
             <!-- Enlaces para Iniciar Sesión y Registro -->
             <a href="login.php">Iniciar Sesión</a>
-            <a href="registro.php">Registrarse</a>
+            <a href="registro.php">Registro</a>
         </div>
+        <?php
+        # Si el usuario ha iniciado sesión escondemos este menu
+        if ($id_user != 0) {
+            echo HIDECLOSE;
+        }
+        ?>
+
+        <?php
+        # Si el usuario NO ha iniciado sesión escondemos este menu
+        if ($id_user == 0) {
+            echo HIDE;
+        }
+        ?>
+        <div id="cerrar_sesion">
+            Hola, <?php echo @$datosUsuario['username'] ?>. <a href="do_logout.php">Cerrar Sesión</a>
+        </div>
+        <?php
+        # Si el usuario NO ha iniciado sesión escondemos este menu
+        if ($id_user == 0) {
+            echo HIDECLOSE;
+        }
+        ?>
     </header>
 </body>
 </html>
--- login.php
+++ login.php
@@ -9,6 +9,8 @@
 </head>
 <body>
     <?php
+    # Cargar constantes
+    require 'constantes.php';
     # Cargamos cookies
     require 'obtenerCookies.php';
     ?>
@@ -19,7 +21,7 @@
     <?php
     # Si el usuario no es invitado escondemos el form de login
     if ($type_user != 'invitado') {
-         echo "<span class='hidden'>";
+         echo HIDE;
      }
     ?>
     <div id="form_login">
@@ -29,30 +31,31 @@
         <input type="text" name="login" required/><br/>
         Contraseña:<br/>
         <input type="password" name="passwd" required/><br/>
-        <input type="submit" name="Iniciar Sesión"/>
+        <input type="submit" name="Iniciar Sesión"/><br/>
+        <a href="index.php">Volver al inicio</a>
     </form>    
     </div>
     <?php
     # Si el usuario no es invitado escondemos el form de login
     if ($type_user != 'invitado') {
-         echo "</span>";
+         echo HIDECLOSE;
      }
     ?>
 
     <?php
     # Si el usuario es invitado escondemos el mensaje de error
     if ($type_user == 'invitado') {
-         echo "<span class='hidden'>";
+         echo HIDE;
      }
     ?>
     <div id="login_error">
-        Ya has iniciado sesión
+        Ya has iniciado sesión. <a href="index.php">Volver al inicio</a>
         <!-- Error cuando ya has iniciado sesión -->
     </div>
     <?php
     # Si el usuario es invitado escondemos el mensaje de error
     if ($type_user == 'invitado') {
-         echo "<span class='hidden'>";
+         echo HIDECLOSE;
      }
     ?>
 
