e0ccfb8f303c56c0950f09c57eecf7f5e967daab
9c23a5 <40715125+9c23a5@users.noreply.github.com>
2021-11-30 10:06:02 +0100

    arreglar headers

--- inc/header.php
+++ inc/header.php
@@ -5,6 +5,12 @@
 
 $datosUser = obtenerDatosUser($id_user);
 ?>
+<head>
+    <link rel="stylesheet" href="css/misc.css" type="text/css"/>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <header>
     <div id="logo">
         <!-- Un img con el logo de TindWare y un canvas con el nombre -->
