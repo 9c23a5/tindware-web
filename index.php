d18677c8ba8b91abe68b38e3ebdf94751aa40627
JRMask <jose06villalba07@gmail.com>
2021-12-02 08:13:07 +0100

    prueba div ofertas

--- index.php
+++ index.php
@@ -15,5 +15,125 @@
     <div id="main">
         <!-- Aqui va el contenido despues del header -->
     </div>
+
+
+
+
+    
+<div id="contenido">
+    <h1>Ultimas noticias</h1>
+    
+    <div id="noticias">
+<h2><a href="noticia1.html">La estructura y los detalles de la temporada de Call of Duty League 2021 finalmente revelados</a></h2>
+<p>Es muy emocionante traerles algunas noticias oficiales verdaderas para la temporada 2021 de Call of Duty League despu�s de haber esperado tanto tiempo. Activision finalmente ha establecido la estructura y los detalles generales de la temporada, revelando c�mo funcionar� la temporada tan esperada</p>
+</div>
+
+
+
+<div class="noticia2">
+    
+    <h2><a href="noticia2.html">Rekkles encuentra su ajuste perfecto cuando se une a G2 Esports</a></h2>
+<p>El regreso del League of Legends competitivo a Europa en forma de LEC ha recuperado el hype del fichaje de Rekkles por G2 como si se hubiera realizado hace solo unas horas, un inicio soberbio del sueco para demostrar con un 25/1/22 que en solo tres partidas los fans ya pueden calmarse pensando en los Worlds 2021.</p>
+
+</div>
+
+<div class="noticia3">
+    
+    <h2><a href="noticia3.html">La muerte cuerpo a cuerpo con un solo golpe de Warzone confunde a la comunidad</a></h2>
+<p>El cuerpo a cuerpo en Call of Duty es salvaje. En el CoD m�s tradicional, una pu�alada es suficiente para derribar a alguien. Sin embargo, hay momentos en Warzone en los que tienes que pelear contra la gente con tus pu�os, y no deber�a ser posible darle un solo golpe a alguien de esta manera. Sin embargo, con algunos jugadores que ven muertes cuerpo a cuerpo de un solo golpe, tenemos que preguntarnos si esta es una nueva tecnolog�a que la gente est� descubriendo o un error horrible que la gente est� viendo por primera vez.</p>
+
+</div>
+     
+    
+
+    
+    <div id="izquierda">
+        
+    
+    <h2>Rankings</h2>
+   
+        <div id="menu2">
+        <ul>
+        <li><a href="https://www.leagueofgraphs.com/es/rankings/rank-distribution">LOL</a></li>
+        <li><a href="https://callofdutyleague.com/en-us/standings">CALL OF DUTY</a></li>
+        <li><a href="https://www.hltv.org/ranking/teams/2021/january/25">CSGO</a></li>
+        <li><a href="https://www.vlr.gg/rankings">VALORANT</a></li>
+        <li><a href="https://fortniterankings.com/">FORNITE</a></li>
+    </ul> 
+        
+        
+    <h2>Destacadas de la Semana</h2>
+   
+        <div id="menu2">
+        <ul>
+        <li><a href="https://www.esportstalk.com/blog/what-we-want-to-see-from-black-ops-cold-war-season-2/">Lo que queremos ver de la temporada 2 de Black Ops Cold War</a></li><br>
+        <li><a href="https://www.esportstalk.com/blog/fortnite-season-5-week-12-challenges-guide/">Gu�a de desaf�os de la semana 12 de la temporada 5 de Fortnite </a></li>
+        <br>
+            <li><a href="https://www.esportstalk.com/blog/call-of-duty-league-2021-week-1-results-atlanta-faze-la-thieves-dominate/">Resultados de la semana 1 de Call of Duty League 2021: Atlanta FaZe y LA Thieves dominan
+
+</a></li>
+       
+    </ul>
+        
+    </div>
+
+</div>
+        
+
+     </div>   
+        
+        
+        
+
+</div>
+
+<div class="noticia6">
+    
+    <h2><a href="noticia4.html">�Dise�ar� el Dr. Disrespect un nuevo mapa para Valorant?</a></h2>
+<p> Valorant sigue cobrando fuerza, por as� decirlo. Muchos jugadores profesionales de CSGO han renunciado a su juego principal para venir a jugarlo, y jugadores como Shroud lo eligieron de nuevo en lugar de CSGO, debido a que hab�a demasiados tramposos en el primero. Pero �qu� pasa con los mapas? Dr Disrespect tuvo un gran �xito al crear un mapa para Rogue Company, y el dos veces campe�n se acerc� a Riot Games, con el deseo de dise�ar un mapa Valorant para ellos tambi�n. �Pero la empresa morder� el anzuelo? Si son inteligentes, creemos que lo haran.
+</p>
+</div>
+
+
+ <div class="noticias4">
+<h2><a href="#">Se revelan los detalles del robo carmes� de R6 Siege (operador, reelaboraci�n del mapa)</a></h2>
+<p>A medida que Neon Dawn llega a su fin, no tendremos que esperar a la pr�xima ca�da de contenido importante en R6 Siege. As� es, Y6S1 comienza con Crimson Heist en R6 Siege, y tenemos los detalles que necesita saber. Todo se reducir� el 21 de febrero, y con un nuevo Operador (Flores) y un redise�o del mapa (Borde), habr� mucho que ver y hacer mientras el mundo de Rainbow Six contin�a evolucionando.
+
+</p>
+</div>
+
+
+
+<div class="noticia5">
+    
+    <h2><a href="#">Avance del parche 2.1 de League of Legends Wild Rift</a></h2>
+<p>Si est�s tan enamorado de League of Legends: Wild Rift como nosotros, entonces te espera un regalo: �Riot acaba de revelar sus planes para los pr�ximos meses! Y, no hace falta decirlo, �son bastante emocionantes! Hemos podido echar un vistazo al parche 2.1 que durar� desde febrero hasta finales de marzo. Por lo tanto, todo lo que lea a continuaci�n se publicar� y enviar� a servidores activos durante ese per�odo.
+</p>
+
+
+</div>
+
+<div class="gameplay">
+    
+    <h2>GamePlay de la Semana</h2>
+    
+    <iframe width="650" height="300" src="https://www.youtube.com/embed/6xtwPtjKN4Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
+    
+</div>
+        
+        
+
+
+     
+    
+
+    
+    
+
+    
+    
+    
+    
 </body>
 </html>
