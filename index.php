7838d51da262b4f3176f69fd602138776eeba1eb
anthonyMU <anthonyrodriguezmurriel@gmail.com>
2021-12-02 09:02:55 +0100

    colocando_texto

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <title>TindWare - Inicio</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 <body>
     <?php
@@ -14,6 +15,29 @@
     ?>
     <div id="main">
         <!-- Aqui va el contenido despues del header -->
+        <div id="principal">
+
+            <h1>El servicio técnico que no duerme</h1>
+
+            <p>Dedicamos todos nuestros esfuerzos a enlazar los mejores técnicos en cada campo de la informática con usuarios y empresas que necesiten en un momento crucial la asistencia.</p>
+            <p>Creemos que el tiempo es oro, y por eso nuestros técnicos están capacitados para trabajar de la manera más rápida y eficiente.</p>
+
+            <h2>¿Por qué trabajar con nosotros?</h2>
+
+                <p>En nuestra compañia contamos con profesionales en todas las ramas de la informatica.</p>
+
+
+
+        </div>
+        <div id="secundario">
+
+
+
+
+        </div>
+
+
+
     </div>
 
 
