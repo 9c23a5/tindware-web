f0cc85749696f4e6a1a4a14acf754cfe6a3a775d
anthonyMU <anthonyrodriguezmurriel@gmail.com>
2021-12-02 09:57:24 +0100

    texto en index y qs

--- quienessomos.php
+++ quienessomos.php
@@ -8,50 +8,45 @@
     <link rel=stylesheet href="css/qs.css" type="text/css">
 </head>
 
-<header>
 
-        <!-- aqui va el menú y las polladas-->
-
-        <h1>EL MENÚ</h1>
-
-
-    </header>
 
 <body>
 
-    <h1>¿Quiénes somos?</h1>
+    <?php
+    # Cargar constantes
+    include 'inc/constantes.php';
+    # Cargamos cookies
+    include 'inc/obtenerCookies.php';
+    # Incluir header
+    include 'inc/header.php';
+    ?>
 
-    <div id="qs">
+    
+
+    <div id="principal">
+        <h1>¿Quiénes somos?</h1>
         
         <p>¡Hola! Encantado de conocerte, soy el CEO de TINDWARE y te voy a contar de qué trata nuestra empresa, pero antes una pequeña historia...</p>
-        Un dia estabamos todos los fundadores tomando un café, en una modesta cafetería de Sestao, cuando uno de los fundadores comentó que uno de sus amigos estaba sufriendo continuos pantallazos azules en su máquina. 
-        La solución de esta persona hubiera sido, lógicamente, llevarlo a un técnico, pero siendo un día festivo..¿Quién podría ayudarlo? 
+        <p>Un dia estabamos todos los fundadores tomando un café, en una modesta cafetería de Sestao, cuando uno de los fundadores comentó que uno de sus amigos estaba sufriendo continuos pantallazos azules en su máquina.
+        La solución de esta persona hubiera sido, lógicamente, llevarlo a un técnico, pero siendo un día festivo..¿Quién podría ayudarlo?
         Entonces caímos en que no existía un servicio técnico 24h, los 365 días del año, se nos encendió la famosa bombilla. </p>
-        <p>¿Y si ofrecemos un servicio jamás visto antes?
-        Creíamos que sería una locura, pero aquí te presento TINDWARE, el servicio técnico que no duerme.</p>
+        <p>¿Y si ofrecemos un servicio jamás visto antes? Creíamos que sería una locura, pero aquí te presento TINDWARE, el servicio técnico que no duerme.</p>
         <p>Dedicamos todos nuestros esfuerzos a enlazar los mejores técnicos en cada campo de la informática con usuarios y empresas que necesiten en un momento crucial la asistencia.
         Creemos que el tiempo es oro, y por eso nuestros técnicos están capacitados para trabajar de la manera más rápida y eficiente.</p>
 
             <h1>Conocenos en profundidad</h1>
 
-        <p>Si, somos una empresa que ofrece sus servicios al mantenimiento informatico pero...¿sólo eso? Si la respuesta ha sido no, estás en lo cierto y yo te lo voy a enseñar</p>
+        <p>Si, somos una empresa que ofrece sus servicios al mantenimiento informatico pero...¿sólo eso? Si la respuesta ha sido no, estás en lo cierto y yo te lo voy a enseñar
 
-        ¿Quieres darte de alta en nuestro soporte técnico?¿Quieres trabajar con nosotros? Te dejamos un formulario de contacto, en el indicanos si quieres ser cliente o buscas empleo, y cualquier duda que tengas, contactaremos con usted con la información requerida.
+        ¿Quieres darte de alta en nuestro soporte técnico?¿Quieres trabajar con nosotros? Te dejamos un formulario de contacto, en el indicanos si quieres ser cliente o buscas empleo, y cualquier duda que tengas, contactaremos con usted con la información requerida.</p>
        <p> [PINCHA AQUI PARA REDIRIGIR AL FORMULARIO]</p>
     </div>
 
-    <p>??</p>
 
-    
+
 
     
 </body>
-
-<footer>
-
-<h1>El puto pie de página</h1>
-
-</footer>
 
 
 
