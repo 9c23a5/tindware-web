ba6f5963a155dbad14288b0f65d24fdc5835991d
anthonyMU <anthonyrodriguezmurriel@gmail.com>
2021-12-02 14:03:42 +0100

    mas texto para qs

--- index.php
+++ index.php
@@ -17,30 +17,12 @@
         <!-- Aqui va el contenido despues del header -->
         <div id="principal">
 
-            <h2>El servicio técnico que no duerme</h2>
+            <h1>El servicio técnico que no duerme</h1>
 
             <p>Dedicamos todos nuestros esfuerzos a enlazar los mejores técnicos en cada campo de la informática con usuarios y empresas que necesiten en un momento crucial la asistencia.</p>
             <p>Creemos que el tiempo es oro, y por eso nuestros técnicos están capacitados para trabajar de la manera más rápida y eficiente.</p>
-
-            <h2>¿Por qué trabajar con nosotros?</h2>
-
-                <p>En nuestra compañia contamos con profesionales en todas las ramas de la informatica.</p>
-                <p>Te vamos a detallar los campos en los que desarrollamos nuestras actividades, si alguna te llama la atención no dudes en ponerte en contacto con nosotros.</p>
-
-            <h2>Mantenimiento de hardware</h2>
-                <p>Servicio de mantenimiento y reparacion de hardware y perifericos(teclados, ratones, switches,etc...)</p>
-                <img src="img/prueba.jpg" width="100px" height="100px">
-
-            <h2>Diseñadores Web</h2>
-                <p>Mediante lenguajes como css, html, javascript, y framewoks como jquery, etc.
-                    Se encargarán de dar soporte y desarrollar paginas web según las indicaciones del cliente.
-                <p>Para ello, contamos con varios campos en los que seccionamos a nuestros empleados:</p> 
-                <ul>
-                    <li>UX developer</li>
-                    <li>Web Developer</li>
-                    <li>Marketing Online</li>
-                </ul> 
-                <p>Cada uno de nuestros diseñdores webs se encuentran segmentados según su experiencia, desde traine hasta senior.</p>
+                
+           
         </div>
         <div id="secundario">
 
