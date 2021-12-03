3ba8496bd6f417992209d9f11ad3b2539664013d
9c23a5 <40715125+9c23a5@users.noreply.github.com>
2021-12-03 21:41:11 +0100

    eliminar funcion no utilizada

--- scripts/loc.js
+++ scripts/loc.js
@@ -21,25 +21,3 @@ async function obtenerLatLon() {
         navigator.geolocation.getCurrentPosition(geoloc_ok, geoloc_error);
     });
 }
-
-async function realizarGeocoding(query) {
-    var apikey = "2a6ef58c88ad4fd9810b422c5348c25e" // en produccion esto no deberia estar asi c:;
-    var requestOptions = {
-        method: "GET",
-    };
-
-    const posicion = await fetch("https://api.geoapify.com/v1/geocode/search?text=" + query + "&format=json&limit=1&bias=countrycode:es&apiKey=" + apikey, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-        console.log(result)
-        posicion = {}
-        posicion["formatted"] = result.results[0].formatted;
-        posicion["lat"] = result.results[0].lat;
-        posicion["lon"] = result.results[0].lon;
-        console.log(posicion)
-        return posicion;
-    })
-    .catch(error => console.log('error', error));
-
-    console.log(posicion)
-}
