572e8760f2dd2fbc270a96eebb4decaecc47faf4
Anthony <anthonyrodriguezmurriel@gmail.com>
2021-12-09 12:25:39 +0100

    quienessomosfinal

---
+++ qsantiguo.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel=stylesheet href="css/qs.css" type="text/css">
+</head>
+
+
+
+<body>
+
+    <?php
+    # Cargar constantes
+    include 'inc/constantes.php';
+    # Cargamos cookies
+    include 'inc/obtenerCookies.php';
+    # Incluir header
+    include 'inc/header.php';
+    ?>
+
+    
+
+    <div id="principal">
+        <h2>¿Quiénes somos?</h2>
+        <p>¡Hola! Encantado de conocerte, soy el CEO de TINDWARE y te voy a contar de qué trata nuestra empresa, pero antes una pequeña historia...</p>
+        <p>Un dia estabamos todos los fundadores tomando un café, en una modesta cafetería de Sestao, cuando uno de los fundadores comentó que uno de sus amigos estaba sufriendo continuos pantallazos azules en su máquina.
+        La solución de esta persona hubiera sido, lógicamente, llevarlo a un técnico, pero siendo un día festivo..¿Quién podría ayudarlo?
+        Entonces caímos en que no existía un servicio técnico 24h, los 365 días del año, se nos encendió la famosa bombilla. </p>
+        <p>¿Y si ofrecemos un servicio jamás visto antes? Creíamos que sería una locura, pero aquí te presento TINDWARE, el servicio técnico que no duerme.</p>
+        <p>Dedicamos todos nuestros esfuerzos a enlazar los mejores técnicos en cada campo de la informática con usuarios y empresas que necesiten en un momento crucial la asistencia.
+        Creemos que el tiempo es oro, y por eso nuestros técnicos están capacitados para trabajar de la manera más rápida y eficiente.</p>
+
+        <h2>¿Por qué nuestro servicio?</h2>
+        <p>Nos centramos en ofrecer un servicio de calidad, orientado hacia todas las personas. Creemos que es importante que una sociedad tenga
+        conocimientos en informática, ya que un futuro completamente informatizado está a la vuelta de la esquina.
+        Por tanto, estamos plantando la semilla de un futuro sin preocupaciones, en el cuál un simple mensaje de texto o una llamada teléfonica
+        ponga a tu disposicion a los mejores técnicos del mercado.</p>
+        <p>Además queremos ser unfa fuente de oprtunidades para aquellas personas con dificultades para encontrar un empleo pueda tener un oportunidad en este mercado en 
+            expansión</p>
+            
+        <h2>Ofecemos todos los servicios en el campo de la informática.</h2>
+            <p>Como hemos comentado anteriormente, ofrecemos servicios de mantenimiento hardware, desarrollo web, entre otros, orientado
+            tanto a particulares como a empresas. </p>
+            <p>Sin necesitas más información, no dudes en ponerte en contacto con nosotros.</p>   
+
+            <div id="foto">
+                <h1 class="centro">Los fundadores de la empresa</h1>  
+             <img src="img/photo.jpg" alt="Los fundadores" width="500" height="500">
+            </div>
+          
+    </div>
+    
+
+    <div id="secundario">
+
+    <h2>Más información</h2>
+
+    <center>
+        <form>
+            <table border="1">
+                <tr>
+                    <td>
+                        <table>
+                            <tr>
+                                <td>Correo electrónico*</td>
+                                <td><input type="email" name="email"></td>                                
+                            </tr>
+                            <tr>
+                            <td>Observaciones</td>
+                            <td><textarea name="observaciones" cols="18" rows="5"></textarea></td>
+                            </tr>
+                            <tr><td colspan="2" ><input type="submit" value="Enviar" class="submit"></td></tr>
+
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+
+
+    </div>
+
+    <div id="redes">
+
+        <h2>Siguenos en Twitter</h2>
+
+        <a class="twitter-timeline" href="https://twitter.com/TindWare?ref_src=twsrc%5Etfw">Tweets by TindWare</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
+
+    </div>
+
+</body>
+
+</html>
--- quienessomos.php
+++ quienessomos.php
@@ -23,78 +23,82 @@
 
     
 
-    <div id="principal">
+    <div id="qs">
+        
         <h2>¿Quiénes somos?</h2>
-        <p>¡Hola! Encantado de conocerte, soy el CEO de TINDWARE y te voy a contar de qué trata nuestra empresa, pero antes una pequeña historia...</p>
-        <p>Un dia estabamos todos los fundadores tomando un café, en una modesta cafetería de Sestao, cuando uno de los fundadores comentó que uno de sus amigos estaba sufriendo continuos pantallazos azules en su máquina.
-        La solución de esta persona hubiera sido, lógicamente, llevarlo a un técnico, pero siendo un día festivo..¿Quién podría ayudarlo?
-        Entonces caímos en que no existía un servicio técnico 24h, los 365 días del año, se nos encendió la famosa bombilla. </p>
-        <p>¿Y si ofrecemos un servicio jamás visto antes? Creíamos que sería una locura, pero aquí te presento TINDWARE, el servicio técnico que no duerme.</p>
-        <p>Dedicamos todos nuestros esfuerzos a enlazar los mejores técnicos en cada campo de la informática con usuarios y empresas que necesiten en un momento crucial la asistencia.
-        Creemos que el tiempo es oro, y por eso nuestros técnicos están capacitados para trabajar de la manera más rápida y eficiente.</p>
-
-        <h2>¿Por qué nuestro servicio?</h2>
-        <p>Nos centramos en ofrecer un servicio de calidad, orientado hacia todas las personas. Creemos que es importante que una sociedad tenga
-        conocimientos en informática, ya que un futuro completamente informatizado está a la vuelta de la esquina.
-        Por tanto, estamos plantando la semilla de un futuro sin preocupaciones, en el cuál un simple mensaje de texto o una llamada teléfonica
-        ponga a tu disposicion a los mejores técnicos del mercado.</p>
-        <p>Además queremos ser unfa fuente de oprtunidades para aquellas personas con dificultades para encontrar un empleo pueda tener un oportunidad en este mercado en 
-            expansión</p>
-            
-        <h2>Ofecemos todos los servicios en el campo de la informática.</h2>
-            <p>Como hemos comentado anteriormente, ofrecemos servicios de mantenimiento hardware, desarrollo web, entre otros, orientado
-            tanto a particulares como a empresas. </p>
-            <p>Sin necesitas más información, no dudes en ponerte en contacto con nosotros.</p>   
-
-            <div id="foto">
-                <h1 class="centro">Los fundadores de la empresa</h1>  
-             <img src="img/photo.jpg" alt="Los fundadores" width="500" height="500">
-            </div>
+            <p>¡Hola! Encantado de conocerte, soy el CEO de TINDWARE y te voy a contar de qué trata nuestra empresa, pero antes una pequeña historia...</p>
+            <p>Un dia estabamos todos los fundadores tomando un café, en una modesta cafetería de Sestao, cuando uno de los fundadores comentó que uno de sus amigos estaba sufriendo continuos pantallazos azules en su máquina.
+            La solución de esta persona hubiera sido, lógicamente, llevarlo a un técnico, pero siendo un día festivo..¿Quién podría ayudarlo?
+            Entonces caímos en que no existía un servicio técnico 24h, los 365 días del año, se nos encendió la famosa bombilla. </p>
+            <p>¿Y si ofrecemos un servicio jamás visto antes? Creíamos que sería una locura, pero aquí te presento TINDWARE, el servicio técnico que no duerme.</p>
+            <p>Dedicamos todos nuestros esfuerzos a enlazar los mejores técnicos en cada campo de la informática con usuarios y empresas que necesiten en un momento crucial la asistencia.
+            Creemos que el tiempo es oro, y por eso nuestros técnicos están capacitados para trabajar de la manera más rápida y eficiente.</p>
           
     </div>
-    
+    <div id="img1">
 
-    <div id="secundario">
-
-    <h2>Más información</h2>
-
-    <center>
-        <form>
-            <table border="1">
-                <tr>
-                    <td>
-                        <table>
-                            <tr>
-                                <td>Correo electrónico*</td>
-                                <td><input type="email" name="email"></td>                                
-                            </tr>
-                            <tr>
-                            <td>Observaciones</td>
-                            <td><textarea name="observaciones" cols="18" rows="5"></textarea></td>
-                            </tr>
-                            <tr><td colspan="2" ><input type="submit" value="Enviar" class="submit"></td></tr>
-
-                        </table>
-                    </td>
-                </tr>
-            </table>
-        </form>
-    </center>
-
+        <img src="img/tindware.png" alt="Los fundadores" width="500" height="400">
 
     </div>
+    <div id="qns">
+        <h2>¿Por qué nuestro servicio?</h2>
+            <p>Nos centramos en ofrecer un servicio de calidad, orientado hacia todas las personas. Creemos que es importante que una sociedad tenga
+            conocimientos en informática, ya que un futuro completamente informatizado está a la vuelta de la esquina.
+            Por tanto, estamos plantando la semilla de un futuro sin preocupaciones, en el cuál un simple mensaje de texto o una llamada teléfonica
+            ponga a tu disposicion a los mejores técnicos del mercado.</p>
+            <p>Además queremos ser unfa fuente de oprtunidades para aquellas personas con dificultades para encontrar un empleo pueda tener un oportunidad en este mercado en 
+                expansión</p>
+    </div>
+    <div id="img2">
 
-    <div id="redes">
-
-        <h2>Siguenos en Twitter</h2>
-
-<<<<<<< HEAD
-       <a class="twitter-timeline" data-lang="es" data-width="500" data-height="500" data-theme="dark" href="https://twitter.com/TindWare">Tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
-=======
-        <a class="twitter-timeline" href="https://twitter.com/TindWare?ref_src=twsrc%5Etfw">Tweets by TindWare</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
->>>>>>> 2072caa3ca6411da70af338f73a85cbce0eca469
+        <img src="img/servicio.jpg" alt="Los fundadores" width="550" height="400">
 
     </div>
+    <div id="sci">
+        <h2>Ofecemos todos los servicios en el campo de la informática.</h2>
+            <p>Como hemos comentado anteriormente, ofrecemos servicios de mantenimiento hardware, desarrollo web, entre otros, orientado
+            tanto a particulares como a empresas. Un auténtico soporte 365 dias al año, distinto, real. </p>
+            <p>Sin necesitas más información, no dudes en ponerte en contacto con nosotros.</p>  
+            <div id="secundario">
+    <form>
+        <table>
+            <tr>
+                <td>
+                    <table>
+                        <tr>
+                            <td>Correo electrónico*</td>
+                            <td><input type="email" name="email"></td>                                
+                        </tr>
+                        <tr>
+                        <td>Observaciones</td>
+                        <td><textarea name="observaciones" cols="18" rows="5"></textarea></td>
+                        </tr>
+                        <tr><td colspan="2" ><input type="submit" value="Enviar" class="submit"></td></tr>
+
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </form>
+
+
+
+</div>
+
+    </div>
+    <div id="img3">
+
+        <img src="img/servicio2.jpg" alt="Los fundadores" width="600" height="300">
+
+    </div>          
+    <div id="img4">
+                <h2>Los fundadores de la empresa</h2>
+                
+             <img src="img/photo.jpg" alt="Los fundadores" width="500" height="500">
+            
+    </div>
+ 
+
 
 </body>
 
