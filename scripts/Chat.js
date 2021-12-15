ed280cf34db95f02e9f4b50ae200e9286f558f06
9c23a5 <40715125+9c23a5@users.noreply.github.com>
2021-12-15 12:57:34 +0100

    chat listo

--- scripts/Chat.js
+++ scripts/Chat.js
@@ -1,5 +1,8 @@
 // https://dashboard.tawk.to/?lang=es#/chat
-var Tawk_API="eceb663ed2f6b0ef2ec790a4e80bb67490195b94"||{}, Tawk_LoadStart=new Date();
+// email: tinddware@gmail.com
+// passw: waretind1234
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
@@ -7,4 +10,3 @@ s1.src='https://embed.tawk.to/61b7358780b2296cfdd16f62/1fmpqi6d1';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-
