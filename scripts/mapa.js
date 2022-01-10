e3e82e575f4e377681b51490e3bb1052bec5e34e
9c23a5 <40715125+9c23a5@users.noreply.github.com>
2022-01-10 09:16:20 +0100

    arreglado mapa

--- scripts/mapa.js
+++ scripts/mapa.js
@@ -1,3 +1,29 @@
+async function obtenerLatLon() {
+    
+    function geoloc_ok(pos) {
+        $("#gmap_antes").hide();
+        lat = pos.coords.latitude;
+        lon = pos.coords.longitude;
+        console.log("Localizacion permitida");
+        var objeto = {};
+        objeto["lat"] = lat;
+        objeto["lon"] = lon;
+        // updateGMaps(lat, lon);
+        console.log("objeto:", objeto)
+        return objeto;
+    }
+    
+    function geoloc_error(err) {
+        console.log(err.message);
+    }
+
+    return new Promise((geoloc_ok, geoloc_error) => {
+        navigator.geolocation.getCurrentPosition(geoloc_ok, geoloc_error);
+    }).catch(function(error) {
+        console.log(error);
+    });
+}
+
 function updateGMaps(lat, lon, query) {
     var urlTemplate = "https://maps.google.com/maps?q=QUERY&t=&z=13&ie=UTF8&iwloc=&output=embed";
     if (query === undefined) {
@@ -5,6 +31,7 @@ function updateGMaps(lat, lon, query) {
     }
     var url = urlTemplate.replace('QUERY', query);
     $("#gmap").attr('src', url);
+    $("#gmap_antes").hide();
     $("#gmap").show();
 }
 
@@ -43,19 +70,23 @@ $(function(){
     });
 });
 
-$(document).ready(function() {
+$(document).ready(async function() {
     $("#gmap").hide();
     $("#gmap_nosoporte").hide();
     $("#gmap_noseguro").hide();
 
     if (window.isSecureContext) {
         if (navigator.geolocation) {
-            const {lat, lon, error} = obtenerLatLon();
-            if (error) {
-                alert("Se ha encontrado el siguiente error: " + eror);
+            console.log('tiene api')
+            const promiseLatLon = await obtenerLatLon();
+            if (!promiseLatLon) {
+                alert("Se ha encontrado el siguiente error: " + error);
             }
             else {
-                document
+                var lat = promiseLatLon.coords.latitude.toString();
+                var lon = promiseLatLon.coords.longitude.toString();
+                console.log(`lat: ${lat} lon: ${lon}`)
+                console.log('updateando')
                 updateGMaps(lat, lon);
             }
         }
