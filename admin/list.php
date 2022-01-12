d4297a7ea4f079e7441e2394a7cee5cb177a8c65
JRMask <95016815+JRMask@users.noreply.github.com>
2022-01-12 21:33:02 +0100

    quiero llorar

--- admin/list.php
+++ admin/list.php
@@ -15,7 +15,7 @@ echo "<table border=1 >";
 echo "<tr bgcolor=grey ><td>Nombre de usuario<td/>";
 echo "<td>Direccion de correo<td/><td>tipo de usuario <td/><td>fecha de alta<td/><td>Ultimo login <td/><tr/>";
 
-while($consultarow=mysqli_fetch_array($consulta2))
+while($fila=mysqli_fetch_array($consulta2))
 {
     echo "<tr><td>".$fila['username']."</td><td>".$fila['email']."</td><td>".$fila['tipo']."</td><td>".$fila['signupdate']."</td><td>".$fila['lastlogin']."</td></tr>";
      echo "<br/>";
