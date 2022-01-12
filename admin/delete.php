b3280b4b4f38d85f56dfd6fdad40719169707120
JRMask <95016815+JRMask@users.noreply.github.com>
2022-01-12 19:56:56 +0100

    cambios delete / ajustes

--- admin/delete.php
+++ admin/delete.php
@@ -9,15 +9,19 @@ mysqli_select_db($conexion,"tindware") or die ("No se ha conectado a la base de 
 $username=$_GET['username'];
 $email=$_GET['email'];
 
-if (empty($username)and empty($email)){
-    echo "Los campos estan vacios<br/>";
-    echo "Recuerde rellenarlos";
-    echo "<a href='Pantalla_inicial.html'>Volver al inicio<a/>";
+$consulta="SELECT * FROM usuario WHERE nombre='$username' and email='$email';";
+$consulta2=mysqli_query($conexion,$consulta);
+$consultarow=mysqli_num_rows($consulta2);
+
+if($consultarow==0)
+{
+  echo "No existe este registro<a href='index.php'> [Volver al inicio]</a>";  
 }
+
 else
 {   $borrar="DELETE FROM usuario WHERE nombre='$username' and email='$email';";
         mysqli_query($conexion,$borrar);
-        echo "Se han eliminado los registros<a href='index.php'>Volver al inicio</a>";
+        echo "Se han eliminado los registros<a href='index.php'> [Volver al inicio]</a>";
      }
      
      
