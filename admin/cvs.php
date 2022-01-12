f5436d750a185130c18428b8b796d8e644ede91a
Anthony <anthonyrodriguezmurriel@gmail.com>
2022-01-12 17:30:50 +0100

    cambios css

---
+++ admin/cvs.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/misc.css"/>
+    <title>Curriculums - Panel Admin</title>
+    <link rel="stylesheet" href="pa.css" type="text/css"/>
+</head>
+<?php
+@include "inc/header.php";
+
+?>
+<body>
+
+    <div id="main">
+        <table>
+
+            <tr>
+                <td>
+                    <h1 class="style">Base de Datos de Curriculums</h1>
+                </td>
+            <tr>
+                <td class="style">Seleccione el trabajador:</td>
+            </tr>
+            <tr>
+                <td class="style">Ander:</td>
+                <td>
+                    <a href="cv/CV_ANDER.pdf" download="CV Ander">Descargar</a>
+                </td>
+            </tr>
+            <tr>
+                <td class="style">Anthony:</td>
+                <td>
+                    <a href="cv/cv_anthony.pdf" download="CV Anthony">Descargar</a>
+                </td>
+            </tr>
+            <tr>
+                <td class="style">Asier:</td>
+                <td>
+                    <a href="cv/cv_asier.pdf" download="CV Asier">Descargar</a>
+                </td>
+            </tr>
+            <tr>
+                <td class="style">Borja:</td>
+                <td>
+                    <a href="cv/cv_borja.pdf" download="CV Borja">Descargar</a>
+                </td>
+            </tr>
+            <tr>
+                <td class="style">Jose:</td>
+                <td>
+                    <a href="cv/cv_jose.pdf" download="CV Jose">Descargar</a>
+                </td>
+            </tr>
+
+            <br />
+
+
+
+
+
+
+
+
+
+            <br />
+        </table>
+        <button onclick="history.back()">Volver</button>
+
+    </div>
+</body>
+
+</html>
