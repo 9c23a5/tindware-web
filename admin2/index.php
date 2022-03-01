45a8d4eb5e75fd93fbd2cf9414b283df4e3e42ce
9c23a5 <40715125+9c23a5@users.noreply.github.com>
2022-03-01 11:25:33 +0100

    tooltip boton volver

--- admin2/index.php
+++ admin2/index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 
 <head>
-    <script src="https://unpkg.com/@popperjs/core@2"></script>
+    <script src="../scripts/jquery-3.6.0.min.js"></script>
+    <!-- <script src="https://unpkg.com/@popperjs/core@2"></script> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
     <meta charset="UTF-8">
@@ -20,7 +21,7 @@
 
         <div class="navbar-nav">
         <a class="navbar-brand" href="#">
-            <img src="../img/tindware.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+            <img src="../img/tindware.png" alt="" width="24" height="24" class="d-inline-block align-text-top">
         TindWare
         </a>
             <!-- <a class="nav-item nav-link active" href="#">Inicio</a> -->
@@ -29,10 +30,10 @@
             <a class="nav-item nav-link" id="listar" href="listar.php">Listar</a>
             <a class="nav-item nav-link" id="borrar" href="borrar.php">Borrar</a>
         </div>
-            <form class="d-flex" action="buscar.php" method="POST" style="align-items: baseline;">
+            <form class="d-flex" action="buscar.php" method="POST" style="align-items: stretch;">
                 <input class="form-control me-2" type="search" placeholder="Busqueda simple" aria-label="Search" name="query">
                 <button class="btn btn-info me-4" type="submit">Buscar</button>
-                <div class="me-1"><a href="../"><span class="iconify" data-icon="typcn:arrow-back" style="color: #bfbfbf;" data-width="30"></span></a></div>
+                <div class="me-1"><button data-bs-toggle="tooltip" data-bs-placement="bottom" title="Volver" class="btn btn-danger"><a href="../"><span class="iconify" data-icon="typcn:arrow-back" style="color: #ffffff;" data-width="24"></span></a></button></div>
             </form>
             
         </div>
@@ -56,10 +57,16 @@
             "borrar.php" : "borrar"
         }
 
-        var element = document.getElementById(paginas[path][1])
+        var element = document.getElementById(paginas[path])
         element.classList.add("active")
+
+        // Activamos tooltips bootstrap
+        $(document).ready(function() {
+            $("body").tooltip({ selector: '[data-bs-toggle=tooltip]' });
+        });
 </script>
 
-
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 </html>
